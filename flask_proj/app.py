17bf41999cebd08e0b3b299a1eba1f344fe06659
Derek Greene <greenede@classwork.engr.oregonstate.edu>
2024-07-22 19:46:10 -0700

    updated

--- flask_proj/app.py
+++ flask_proj/app.py
@@ -20,29 +20,29 @@ def root():
 def designs():
     query = "SELECT * FROM Designs;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = json.dumps(cursor.fetchall())
-    return results
+    results = cursor.fetchall()
+    return render_template('designs.j2', designs=results)
 
 @app.route('/requirements')
 def requirements():
     query = "SELECT * FROM Requirements;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = json.dumps(cursor.fetchall())
-    return results
+    results = cursor.fetchall()
+    return render_template('requirements.j2', requirements=results)
 
 @app.route('/projects')
 def projects():
     query = "SELECT * FROM Projects;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = json.dumps(cursor.fetchall())
-    return results
+    results = cursor.fetchall()
+    return render_template('projects.j2', projects=results)
 
 @app.route('/users')
 def users():
     query = "SELECT * FROM Users;"
     cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = json.dumps(cursor.fetchall())
-    return results
+    results = cursor.fetchall()
+    return render_template('users.j2', users=results)
 
 # Listener
 if __name__ == "__main__":
