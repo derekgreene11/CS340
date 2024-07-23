48f631a425b1c2a0c74e09780bfe72f893fd2ef2
Derek Greene <greenede@classwork.engr.oregonstate.edu>
2024-07-23 07:42:10 -0700

    updates

--- flask_proj/app.py
+++ flask_proj/app.py
@@ -9,12 +9,11 @@ app = Flask(__name__)
 db_connection = db.connect_to_database()
 mysql = MySQL(app)
 
-
 # Routes
 @app.route('/')
 def root():
 
-    return "<h1>Working</h1>"
+    return "<h1>Engineering Services Database 1.0</h1>"
 
 @app.route('/designs')
 def designs():
@@ -43,6 +42,38 @@ def users():
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
     return render_template('users.j2', users=results)
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        discipline = request.form['discipline']
+        query = "INSERT INTO Users (firstName, lastName, discipline) VALUES (%s, %s, %s);"
+        db.execute_query(db_connection=db_connection, query=query, data=(first_name, last_name, discipline))
+        return redirect(url_for('users'))
+    return render_template('add_user.j2')
+
+@app.route('/edit_user/<int:user_id>', methods=['GET', 'POST'])
+def edit_user(user_id):
+    if request.method == 'POST':
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        discipline = request.form['discipline']
+        query = "UPDATE Users SET firstName = %s, lastName = %s, discipline = %s WHERE userId = %s;"
+        db.execute_query(db_connection=db_connection, query=query, data=(first_name, last_name, discipline, user_id))
+        return redirect(url_for('users'))
+    else:
+        query = "SELECT * FROM Users WHERE userId = %s;"
+        cursor = db.execute_query(db_connection=db_connection, query=query, data=(user_id,))
+        user = cursor.fetchone()
+        return render_template('edit_user.j2', user=user)
+
+@app.route('/delete_user/<int:user_id>')
+def delete_user(user_id):
+    query = "DELETE FROM Users WHERE userId = %s;"
+    db.execute_query(db_connection=db_connection, query=query, data=(user_id,))
+    return redirect(url_for('users'))
 
 # Listener
 if __name__ == "__main__":
