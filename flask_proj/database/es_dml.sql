d0caf7e4a98dfb5302be0ba931f5cfc5fbb7edbb
Derek Greene <greenede@classwork.engr.oregonstate.edu>
2024-07-30 11:05:35 -0700

    updates

--- flask_proj/database/es_dml.sql
+++ flask_proj/database/es_dml.sql
@@ -12,36 +12,48 @@ INSERT INTO Projects (projectStatus) VALUES (%s);
 -- Used in the add_user form to add a new user
 INSERT INTO Users (firstName, lastName, discipline) VALUES (%s, %s, %s);
 
+-- Associate a user with a project
+INSERT INTO UserProjects (userId, projectId) VALUES (%s, %s);
+
+-- Associate a requirement with a project
+INSERT INTO ProjectRequirements (projectId, requirementId) VALUES (%s, %s);
+
+-- Insert partNumber and projectID into DesignProjects
+INSERT INTO DesignProjects (partNumber, projectId) VALUES (%s, %s);
+
 -- R(ead):
--- Used to display all the designs
+-- Fetch all designs with associated projects
 SELECT d.partNumber, d.tool, d.revision, p.projectId
 FROM Designs d
 LEFT JOIN DesignProjects dp ON d.partNumber = dp.partNumber
 LEFT JOIN Projects p ON dp.projectId = p.projectId;
 
--- Used to display all the requirements
+-- Fetch all requirements
 SELECT * FROM Requirements;
 
--- Used to display all the users
+-- Fetch all users
 SELECT * FROM Users;
 
--- Used to display all the projects and the users assigned to each
+-- Fetch all users with their ID, first name, and last name
+SELECT userId, firstName, lastName FROM Users;
+
+-- Fetch all projects with their associated users
 SELECT 
+    p.projectId,
+    p.projectStatus,
     u.userId,
     u.firstName,
-    u.lastName,
-    p.projectId,
-    p.projectStatus
+    u.lastName
 FROM 
-    Users u
-JOIN 
-    UserProjects up ON u.userId = up.userId
-JOIN 
-    Projects p ON up.projectId = p.projectId
+    Projects p
+LEFT JOIN 
+    UserProjects up ON p.projectId = up.projectId
+LEFT JOIN 
+    Users u ON up.userId = u.userId
 ORDER BY 
-    u.userId, p.projectId;
+    p.projectId, u.userId;
 
--- Used to display all the requirements and their associated projects
+-- Fetch all requirements with their associated projects
 SELECT 
     r.requirementId,
     r.level,
@@ -56,28 +68,86 @@ LEFT JOIN
 ORDER BY 
     r.requirementId, p.projectId;
 
+-- Fetch all designs by partNumber
+SELECT * FROM Designs WHERE partNumber = %s;
+
+-- Fetch all requirements by requirementId
+SELECT * FROM Requirements WHERE requirementId = %s;
+
+-- Fetch all projects by projectId
+SELECT * FROM Projects WHERE projectId = %s;
+
+-- Fetch all users by userId
+SELECT * FROM Users WHERE userId = %s;
+
+-- Fetch all projects and their associated users
+SELECT p.projectId, p.projectStatus, u.userId, u.firstName, u.lastName
+FROM Projects p
+LEFT JOIN UserProjects up ON p.projectId = up.projectId
+LEFT JOIN Users u ON up.userId = u.userId
+ORDER BY p.projectId, u.userId;
+
+-- Fetch all requirements and their associated projects
+SELECT r.requirementId, r.level, p.projectId, p.projectStatus
+FROM Requirements r
+LEFT JOIN ProjectRequirements pr ON r.requirementId = pr.requirementId
+LEFT JOIN Projects p ON pr.projectId = p.projectId
+ORDER BY r.requirementId, p.projectId;
+
+-- Fetch all part numbers associated with a project
+SELECT partNumber FROM DesignProjects WHERE projectId = %s;
+
+-- Fetch all user IDs associated with a project
+SELECT userId FROM UserProjects WHERE projectId = %s;
+
+-- Fetch all parts associated with a project
+SELECT DISTINCT partNumber FROM Designs WHERE partNumber IN (SELECT partNumber FROM DesignProjects WHERE projectId = %s);
+
+-- Fetch all users associated with a project
+SELECT u.userId, u.firstName, u.lastName FROM Users u
+JOIN UserProjects up ON u.userId = up.userId
+WHERE up.projectId = %s;
+
+-- Fetch all project IDs associated with a requirement
+SELECT projectId FROM ProjectRequirements WHERE requirementId = %s;
+
+-- Fetch all project IDs from Projects
+SELECT projectId FROM Projects;
+
 -- U(pdate):
--- Used in the edit_design form to update a design
+-- Update a design
 UPDATE Designs SET tool = %s, revision = %s WHERE partNumber = %s;
 
--- Used in the edit_requirement form to update a requirement
+-- Update a requirement
 UPDATE Requirements SET level = %s WHERE requirementId = %s;
 
--- Used in the edit_project form to update a project
+-- Update a project
 UPDATE Projects SET projectStatus = %s WHERE projectId = %s;
 
--- Used in the edit_user form to update a user
+-- Update a user
 UPDATE Users SET firstName = %s, lastName = %s, discipline = %s WHERE userId = %s;
 
 -- D(elete):
--- Used to delete a design
+-- Delete a design
 DELETE FROM Designs WHERE partNumber = %s;
 
--- Used to delete a requirement
+-- Delete a design from DesignProjects
+DELETE FROM DesignProjects WHERE partNumber = %s;
+
+-- Delete a requirement
 DELETE FROM Requirements WHERE requirementId = %s;
 
--- Used to delete a project
+-- Delete a requirement from ProjectRequirements
+DELETE FROM ProjectRequirements WHERE requirementId = %s;
+
+-- Delete a project
 DELETE FROM Projects WHERE projectId = %s;
 
--- Used to delete a user
+-- Delete a user
 DELETE FROM Users WHERE userId = %s;
+
+-- Delete all user projects associated with a project
+DELETE FROM UserProjects WHERE projectId = %s;
+
+-- Delete all design projects associated with a project
+DELETE FROM DesignProjects WHERE projectId = %s;
