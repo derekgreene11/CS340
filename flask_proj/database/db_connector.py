ad100963f460745b9afc7ebfc3acb6d9023ac831
Derek Greene <greenede@classwork.engr.oregonstate.edu>
2024-07-30 12:15:15 -0700

    updates

--- flask_proj/database/db_connector.py
+++ flask_proj/database/db_connector.py
@@ -1,15 +1,19 @@
+# Citation for db_connector.py:
+# Date: 6/23//2024
+# Adapted from:
+# Source URL: https://github.com/osu-cs340-ecampus/flask-starter-app/blob/master/database/db_connector.py
+
 import MySQLdb
 import os
 from dotenv import load_dotenv, find_dotenv
 
-# Load environment variables from the .env file
 load_dotenv(find_dotenv())
 
 # Set the variables in application with environment variables
-host = os.environ.get("DBHOST")
-user = os.environ.get("DBUSER")
-passwd = os.environ.get("DBPW")
-db = os.environ.get("ESDB")
+host = os.environ.get("340DBHOST")
+user = os.environ.get("340DBUSER")
+passwd = os.environ.get("340DBPW")
+db = os.environ.get("340DB")
 
 def connect_to_database(host=host, user=user, passwd=passwd, db=db):
     db_connection = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
